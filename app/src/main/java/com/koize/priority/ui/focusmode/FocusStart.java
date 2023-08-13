6845151b6eed1fdb193e4e8ac4d4c45e5c6c9a9c
Drew <drewcfrog@gmail.com>
2023-08-13 11:17:14 +0800

    focus

---
+++ app/src/main/java/com/koize/priority/ui/focusmode/FocusStart.java
@@ -0,0 +1,16 @@
+package com.koize.priority.ui.focusmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.koize.priority.R;
+
+public class FocusStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_focus_start);
+    }
+}
